8a1dda8ba4e4f57aa65cf63d4058417286b8d9cb
Jens Lindekamp <jens.lindekamp@ista.com>
2025-04-07 18:11:04 +0200

    Added tailwind

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Activate environment on Windows with Git Bash
     source .venv/Scripts/activate
 
+# Run server (development mode)
+* `py manage.py runserver` will run the application
+* `py manage.py tailwind start` will watch the templates and regenerate css if necessary.
 
 # Installation process
 
 * `django-admin startproject haynes .` to initialise an empty django project
-* 
+* Configured `settings.py` to use configuration from file `env`
+* Followed installation on https://django-tailwind.readthedocs.io/en/latest/installation.html
--- haynes/settings.py
+++ haynes/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'haynes.urls'
@@ -130,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+NPM_BIN_PATH = config('NPM_BIN_PATH', default="npm")
